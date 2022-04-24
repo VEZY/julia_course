a819f7935d51a8a534b0b6cec38b76bbb6d8c785
remi.vezy <VEZY@users.noreply.github.com>
2022-04-24 16:01:58 +0200

    Add compound expressions
    + draft for conditional statements and for loops

---
+++ 7-compound_expressions.jl
@@ -0,0 +1,164 @@
+### A Pluto.jl notebook ###
+# v0.18.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 28cc7fa0-c3cf-11ec-0785-0df4a922acf7
+md"""
+## Compound Expressions
+
+A compound expression is a sequence of statements. 
+
+## Begin...end
+
+If you followed through the previous videos of the tutorial, you already made acquaintance with one of them: the `begin...end` statement.
+
+The `begin...end` statement is used when we want to execute several expressions as if they where executed as a single one. The `begin...end` always returns the last evaluated expression.
+
+Let's take an example:
+"""
+
+# ╔═╡ 87084987-db67-4a2a-82f4-6075884e7e54
+x = begin
+	a = 1
+	b = 2
+	a + b
+end
+
+# ╔═╡ 10365f8e-cb88-4205-9d9f-ffe4cc1a8b15
+md"""
+Here we put three lines of code inside the `begin...end` statement. We first define the value of `a`, then the value of `b`, and we evaluate their sum. The result of the sum is then returned by the whole expression and assigned to the variable `x`.
+
+All variables created inside the `begin...end` statement are available to the global scope. We can access the value of `a`:
+"""
+
+# ╔═╡ 53b44815-5157-4d00-b58f-e1850320d147
+a
+
+# ╔═╡ 130967d8-726a-4542-a018-72c359020405
+md"""
+And the value of `b`:
+"""
+
+# ╔═╡ 3e2c281d-e7d3-4cde-b8b1-a89641a32a3e
+b
+
+# ╔═╡ 9e9c6afe-f8bf-453b-87ee-99c32e597d50
+md"""
+And of course the value of `x` too:
+"""
+
+# ╔═╡ 3780dae1-6695-49ff-be65-db6fdc990320
+x
+
+# ╔═╡ bb119f12-a502-4c90-98fe-a04a8def4a77
+md"""
+I know that your next question is probably that we could simply not use the `begin...end` statement at all. In this case yes, we could define it like so instead:
+
+```julia
+a = 1
+b = 2
+x = a + b
+```
+
+And this would be way more readable. But keep in mind that it is only a rather bad example for keeping it simple. You'll see examples where they are quite usefull in further videos. And actually Pluto notebooks are a good example for that, because if you need to put several lines of code inside a single cell, you are required to use a `begin...end` statement.   
+"""
+
+# ╔═╡ 3db818e3-043a-4133-b85c-64a3f3d91530
+md"""
+## Let...end
+
+Another nice expression that we encountered previously is the `let...end` statement. This one is very similar to the `begin...end` statement, but has one more interesting property: all variables created inside are local variables, meaning they are not exposed to the global scope. 
+
+Let's take an example to better understand. Let's define an expression equivalent to the previous one using `let...end`:
+"""
+
+# ╔═╡ 485760ec-e72f-42e3-9a4c-506cbf2fa99a
+y = let
+	c = 1
+	d = 2
+	c + d
+end
+
+# ╔═╡ 64988325-8a5f-4e46-917a-3e289f0b8f5d
+md"""
+Ok, seems to be same no? Yes, but the difference is that this time the `c` and `d` variables are not available to us.
+"""
+
+# ╔═╡ 683e1fbf-a288-43fe-bf99-ee8d89f20596
+c
+
+# ╔═╡ e04f0202-0329-4e16-99a6-b4499ca5722a
+md"""
+To avoid the error we can test if a variable exist in the global scope using the `@isdefined` macro:
+"""
+
+# ╔═╡ e03c5319-1d3e-4132-86db-f9cde880b122
+@isdefined c
+
+# ╔═╡ 57b8706f-a0c4-4c98-8d22-afcae590c405
+@isdefined d
+
+# ╔═╡ 02a43c78-48bd-4b01-95a4-a2486b603c16
+md"""
+Another usefull thing of `let...end` is that we can assign the values of outside variables with the same name inside the `let...end` statement, and the variable inside would be a totally different variable:
+"""
+
+# ╔═╡ b0f51d40-1086-4afa-86cb-dc4ff756d59d
+z = 1
+
+# ╔═╡ 4599c507-64aa-4f25-8cee-2059e54a9c0f
+res = let z = z
+	z = 2
+end
+
+# ╔═╡ 44dbb163-871e-40d7-becf-eb916112f227
+md"""
+What is the value of res know? Well we see it right above, it is 2. Because it is the result of the last evaluated expression inside the `let...end` statement. But what is the value of `z` now?
+
+If you followed, the value of `z` didn't change, because we changed the value of a totally different `z`, the one inside the `let...end` statement that creates a new scope:
+"""
+
+# ╔═╡ f7fd84a2-f53d-40b4-8eda-117f4ac511da
+z
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─28cc7fa0-c3cf-11ec-0785-0df4a922acf7
+# ╠═87084987-db67-4a2a-82f4-6075884e7e54
+# ╟─10365f8e-cb88-4205-9d9f-ffe4cc1a8b15
+# ╠═53b44815-5157-4d00-b58f-e1850320d147
+# ╟─130967d8-726a-4542-a018-72c359020405
+# ╠═3e2c281d-e7d3-4cde-b8b1-a89641a32a3e
+# ╟─9e9c6afe-f8bf-453b-87ee-99c32e597d50
+# ╠═3780dae1-6695-49ff-be65-db6fdc990320
+# ╟─bb119f12-a502-4c90-98fe-a04a8def4a77
+# ╟─3db818e3-043a-4133-b85c-64a3f3d91530
+# ╠═485760ec-e72f-42e3-9a4c-506cbf2fa99a
+# ╟─64988325-8a5f-4e46-917a-3e289f0b8f5d
+# ╠═683e1fbf-a288-43fe-bf99-ee8d89f20596
+# ╟─e04f0202-0329-4e16-99a6-b4499ca5722a
+# ╠═e03c5319-1d3e-4132-86db-f9cde880b122
+# ╠═57b8706f-a0c4-4c98-8d22-afcae590c405
+# ╟─02a43c78-48bd-4b01-95a4-a2486b603c16
+# ╠═b0f51d40-1086-4afa-86cb-dc4ff756d59d
+# ╠═4599c507-64aa-4f25-8cee-2059e54a9c0f
+# ╟─44dbb163-871e-40d7-becf-eb916112f227
+# ╠═f7fd84a2-f53d-40b4-8eda-117f4ac511da
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
---
+++ 8-conditional_statements.jl
@@ -0,0 +1,450 @@
+### A Pluto.jl notebook ###
+# v0.18.0
+
+using Markdown
+using InteractiveUtils
+
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
+        el
+    end
+end
+
+# ╔═╡ 9fe70871-d9d8-419d-b35a-89cfbca298d9
+begin
+	using Dates
+	using PlutoUI
+end
+
+# ╔═╡ 8e7edb2e-c349-11ec-0d3d-9b77143ff4da
+md"""
+# Control flow: conditional statements
+
+Like any other programming language, Julia provides expressions for controlling the execution flow of a program. This notebook is based on the [Julia documentation](https://docs.julialang.org/en/v1/manual/control-flow/).
+
+## If/else
+
+The `if` statement helps us execute some come only if the condition evaluates to `true`. It is written with the form:
+
+```julia
+if condition
+	[...]
+end
+```
+
+with `[...]` being any number of lines of Julia code.
+
+But let's take an example to better understand. Say we go the gym every wednesday, and that we want Julia to remind us when it is time to go. 
+Let's simulate the day with a variable called `today` that numbers days from 1 to 7. We can change the values using the following slider:
+"""
+
+# ╔═╡ fa91bd90-6df6-4fc0-93ec-d3fbd4b65c81
+@bind today PlutoUI.Slider(1:7; show_value = true)
+
+# ╔═╡ 6be3a5b2-8382-41c3-b9dd-8a7b4ae83e34
+if today == Wednesday
+	"Move! Move! Move! It's Wednesday! Gym day!"
+end
+
+# ╔═╡ 1ddfd261-11a0-436f-a47e-077889e8b2cb
+md"""
+See what happens when we move the slider to the third day? The condition ``today == Wednesday` evaluates to `true` so the code inside the if...end expression is evaluated.
+
+But we can make it even better by outputing a value when it is not wednesday. We could add an `if` statement below and test if the day is not wednesday, but an even shorter way to do it is by using the `else` statement as follows:  
+"""
+
+# ╔═╡ b1c91a21-94ee-470a-ad86-ea9d2f60859d
+if today == Wednesday
+	"🏃Move! Move! Move! It's Wednesday! Gym day!🏃"
+else
+	"😴Chill out it is not Wednesday yet😴"
+end
+
+# ╔═╡ a937a97d-1eb1-4322-86f1-f8e8e1b465d5
+md"""
+The `else` statement helps us execute another chunk of code if the condition is `false`. But what if we want to go to the gym on the Mondays too? Well we can use the `elseif` statement like follows:
+"""
+
+# ╔═╡ fd60cfbf-4e4e-4708-98bd-ee3776be6abb
+if today == Monday
+	"🏃Move! Move! Move! It's Monday! Gym day!🏃"
+elseif today == Wednesday
+	"🏃Move! Move! Move! It's Wednesday! Gym day!🏃"
+else
+	"😴Chill out it is not Wednesday yet😴"
+end
+
+# ╔═╡ 524dd17a-21b6-45ae-b0a7-3f4c9322bde0
+md"""
+And the nice thing is that we can chain them as we want:
+"""
+
+# ╔═╡ 21442482-0f68-499a-b072-ac219fbbf0ce
+if today == Monday
+	"🏃Move! Move! Move! It's Monday! Gym day!🏃"
+elseif today == Wednesday
+	"🏃Move! Move! Move! It's Wednesday! Gym day!🏃"
+elseif today == Friday
+	"🏃Move! Move! Move! It's Friday! Gym day!🏃"
+else
+	"😴Chill out it is not Wednesday yet😴"
+end
+
+# ╔═╡ aa66bcad-c9d4-4a05-9e6b-7202f9d4f37b
+md"""
+!!! note
+	In general it is best to avoid repeating code, so a better way to code the above would be:
+	```julia
+
+	if today in [Monday, Wednesday, Friday]
+		"🏃Move! Move! Move! It's \$(Dates.dayname(today)) ! Gym day!🏃"
+	else
+		"😴Chill out it is not Wednesday yet😴"
+	end
+
+	```
+"""
+
+# ╔═╡ f23ae4cb-c511-4c85-b6b6-a3b497e1f0f1
+md"""
+## If / else shortcuts
+
+Sometimes we want to execute something conditionnaly but more shortly than using `if...else...end`. 
+
+There're two ways of doing that:
+
+```julia
+condition ? [code if true] : [code if false]
+```
+
+> We can read this as: is the condition true (**?**), if yes execute what follows, else (**:**) execute the other one.
+
+Here is an example:
+"""
+
+# ╔═╡ 054086d5-9893-46af-bd5d-92c4958eafbe
+today == Wednesday ? "It's Wednesday!" : "It's not wednesday"
+
+# ╔═╡ 4d3c5c93-f37a-451e-9b44-6cda6e5167d8
+md"""
+The expression `today == Wednesday` is first evaluated, if it is `true` the whole expression returns `"It's Wednesday!"`, else it returns `"It's not wednesday"`.
+"""
+
+# ╔═╡ d6b8c30d-a8d3-470b-ba50-90da8ad6bcd8
+md"""
+But what if we don't need the `else` statement? Well there is another shorcut for that one: the `&&` or `||`. The `&&` statement doesn't evaluate the right-hand side of its expression if the left-hand side is `false`, and the `||` only evaluates its right-hand side if the left-hand side is `false`:
+
+```julia
+condition && [code if true]
+```
+
+The statement can be read as:
+
+> If condition is `true`, execute `[code if true]`, else do nothing.
+
+```julia
+condition || [code if false]
+```
+
+The statement can be read as:
+
+> If condition is `false`, execute `[code if false]`, else do nothing.
+
+
+Here are some examples:
+"""
+
+# ╔═╡ 822d86e2-be43-4dec-bbce-279d4c126973
+today == Wednesday && "It's Wednesday!"
+
+# ╔═╡ 469a5e6c-b513-40c0-98b5-32072c2c1f1d
+md"""
+The expression `"today == Wednesday"` is evalutated, if it is `true` the `"It's Wednesday!"` expression is evaluted, and if it is `false`, it doesn't evaluate it and continue.
+"""
+
+# ╔═╡ de5ca094-d8a1-4216-a261-382db303dfad
+today == Wednesday || "It's not Wednesday yet."
+
+# ╔═╡ 0409a95a-fe40-4c3d-9e7e-bb52276c596f
+md"""
+## For loops
+
+For loops helps us execute code several times over. They are written like so:
+
+```julia
+for i in range
+	[...]
+end
+```
+
+The `for` loop can be read as:
+> for each `i` value in `range`, execute the code in `[...]`:
+
+The `range` expression can be any iterable object. It usually a range, a vector, tuple or string. 
+
+Let's start with a simple range example:
+"""
+
+# ╔═╡ ebb84214-1acd-4cbd-a4c3-f0c3584a27d1
+md"""
+What happens here? Well the for loop is executed 3 times, one time for each value in the range 1:3. Then the Julia code inside the loop simply prints the value of i at each iteration, leading to the output: 1 2 3. 
+
+!!! note
+	We wrap our for loop in a `with_terminal` statement because else Pluto wouldn't show the output of `println`. But no need to do this if you're using Julia directly from the terminal or from an IDE like VS Code.
+"""
+
+# ╔═╡ c8d5b5c1-780c-443a-a367-4f76a351eb98
+
+
+# ╔═╡ 9c9d13b9-7ef0-4a2a-ba8f-e605639abc5a
+
+
+# ╔═╡ dab76fe5-5a2d-4a63-ba6c-3b734338b2c0
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+Dates = "ade2ca70-3891-5945-98fb-dc099432e06a"
+PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+
+[compat]
+PlutoUI = "~0.7.38"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[[deps.AbstractPlutoDingetjes]]
+deps = ["Pkg"]
+git-tree-sha1 = "8eaf9f1b4921132a4cff3f36a1d9ba923b14a481"
+uuid = "6e696c72-6542-2067-7265-42206c756150"
+version = "1.1.4"
+
+[[deps.ArgTools]]
+uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
+
+[[deps.Artifacts]]
+uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
+
+[[deps.Base64]]
+uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
+
+[[deps.ColorTypes]]
+deps = ["FixedPointNumbers", "Random"]
+git-tree-sha1 = "024fe24d83e4a5bf5fc80501a314ce0d1aa35597"
+uuid = "3da002f7-5984-5a60-b8a6-cbb66c0b333f"
+version = "0.11.0"
+
+[[deps.CompilerSupportLibraries_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
+
+[[deps.Dates]]
+deps = ["Printf"]
+uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
+
+[[deps.Downloads]]
+deps = ["ArgTools", "LibCURL", "NetworkOptions"]
+uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
+
+[[deps.FixedPointNumbers]]
+deps = ["Statistics"]
+git-tree-sha1 = "335bfdceacc84c5cdf16aadc768aa5ddfc5383cc"
+uuid = "53c48c17-4a7d-5ca2-90c5-79b7896eea93"
+version = "0.8.4"
+
+[[deps.Hyperscript]]
+deps = ["Test"]
+git-tree-sha1 = "8d511d5b81240fc8e6802386302675bdf47737b9"
+uuid = "47d2ed2b-36de-50cf-bf87-49c2cf4b8b91"
+version = "0.0.4"
+
+[[deps.HypertextLiteral]]
+git-tree-sha1 = "2b078b5a615c6c0396c77810d92ee8c6f470d238"
+uuid = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
+version = "0.9.3"
+
+[[deps.IOCapture]]
+deps = ["Logging", "Random"]
+git-tree-sha1 = "f7be53659ab06ddc986428d3a9dcc95f6fa6705a"
+uuid = "b5f81e59-6552-4d32-b1f0-c071b021bf89"
+version = "0.2.2"
+
+[[deps.InteractiveUtils]]
+deps = ["Markdown"]
+uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+
+[[deps.JSON]]
+deps = ["Dates", "Mmap", "Parsers", "Unicode"]
+git-tree-sha1 = "3c837543ddb02250ef42f4738347454f95079d4e"
+uuid = "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
+version = "0.21.3"
+
+[[deps.LibCURL]]
+deps = ["LibCURL_jll", "MozillaCACerts_jll"]
+uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
+
+[[deps.LibCURL_jll]]
+deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
+uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
+
+[[deps.LibGit2]]
+deps = ["Base64", "NetworkOptions", "Printf", "SHA"]
+uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
+
+[[deps.LibSSH2_jll]]
+deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
+uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
+
+[[deps.Libdl]]
+uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
+
+[[deps.LinearAlgebra]]
+deps = ["Libdl", "libblastrampoline_jll"]
+uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+
+[[deps.Logging]]
+uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
+
+[[deps.Markdown]]
+deps = ["Base64"]
+uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
+
+[[deps.MbedTLS_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
+
+[[deps.Mmap]]
+uuid = "a63ad114-7e13-5084-954f-fe012c677804"
+
+[[deps.MozillaCACerts_jll]]
+uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
+
+[[deps.NetworkOptions]]
+uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
+
+[[deps.OpenBLAS_jll]]
+deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
+uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
+
+[[deps.Parsers]]
+deps = ["Dates"]
+git-tree-sha1 = "3b429f37de37f1fc603cc1de4a799dc7fbe4c0b6"
+uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
+version = "2.3.0"
+
+[[deps.Pkg]]
+deps = ["Artifacts", "Dates", "Downloads", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
+uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
+
+[[deps.PlutoUI]]
+deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "Markdown", "Random", "Reexport", "UUIDs"]
+git-tree-sha1 = "670e559e5c8e191ded66fa9ea89c97f10376bb4c"
+uuid = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+version = "0.7.38"
+
+[[deps.Printf]]
+deps = ["Unicode"]
+uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
+
+[[deps.REPL]]
+deps = ["InteractiveUtils", "Markdown", "Sockets", "Unicode"]
+uuid = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
+
+[[deps.Random]]
+deps = ["SHA", "Serialization"]
+uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
+
+[[deps.Reexport]]
+git-tree-sha1 = "45e428421666073eab6f2da5c9d310d99bb12f9b"
+uuid = "189a3867-3050-52da-a836-e630ba90ab69"
+version = "1.2.2"
+
+[[deps.SHA]]
+uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
+
+[[deps.Serialization]]
+uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
+
+[[deps.Sockets]]
+uuid = "6462fe0b-24de-5631-8697-dd941f90decc"
+
+[[deps.SparseArrays]]
+deps = ["LinearAlgebra", "Random"]
+uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
+
+[[deps.Statistics]]
+deps = ["LinearAlgebra", "SparseArrays"]
+uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
+
+[[deps.TOML]]
+deps = ["Dates"]
+uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
+
+[[deps.Tar]]
+deps = ["ArgTools", "SHA"]
+uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
+
+[[deps.Test]]
+deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
+uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
+
+[[deps.UUIDs]]
+deps = ["Random", "SHA"]
+uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
+
+[[deps.Unicode]]
+uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
+
+[[deps.Zlib_jll]]
+deps = ["Libdl"]
+uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
+
+[[deps.libblastrampoline_jll]]
+deps = ["Artifacts", "Libdl", "OpenBLAS_jll"]
+uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
+
+[[deps.nghttp2_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
+
+[[deps.p7zip_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
+"""
+
+# ╔═╡ Cell order:
+# ╠═9fe70871-d9d8-419d-b35a-89cfbca298d9
+# ╟─8e7edb2e-c349-11ec-0d3d-9b77143ff4da
+# ╟─fa91bd90-6df6-4fc0-93ec-d3fbd4b65c81
+# ╠═6be3a5b2-8382-41c3-b9dd-8a7b4ae83e34
+# ╟─1ddfd261-11a0-436f-a47e-077889e8b2cb
+# ╠═b1c91a21-94ee-470a-ad86-ea9d2f60859d
+# ╟─a937a97d-1eb1-4322-86f1-f8e8e1b465d5
+# ╠═fd60cfbf-4e4e-4708-98bd-ee3776be6abb
+# ╟─524dd17a-21b6-45ae-b0a7-3f4c9322bde0
+# ╠═21442482-0f68-499a-b072-ac219fbbf0ce
+# ╟─aa66bcad-c9d4-4a05-9e6b-7202f9d4f37b
+# ╟─f23ae4cb-c511-4c85-b6b6-a3b497e1f0f1
+# ╠═054086d5-9893-46af-bd5d-92c4958eafbe
+# ╟─4d3c5c93-f37a-451e-9b44-6cda6e5167d8
+# ╟─d6b8c30d-a8d3-470b-ba50-90da8ad6bcd8
+# ╠═822d86e2-be43-4dec-bbce-279d4c126973
+# ╟─469a5e6c-b513-40c0-98b5-32072c2c1f1d
+# ╠═de5ca094-d8a1-4216-a261-382db303dfad
+# ╠═0409a95a-fe40-4c3d-9e7e-bb52276c596f
+# ╠═ebb84214-1acd-4cbd-a4c3-f0c3584a27d1
+# ╠═c8d5b5c1-780c-443a-a367-4f76a351eb98
+# ╠═9c9d13b9-7ef0-4a2a-ba8f-e605639abc5a
+# ╠═dab76fe5-5a2d-4a63-ba6c-3b734338b2c0
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002

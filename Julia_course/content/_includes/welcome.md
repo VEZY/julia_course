e46db10d4568fc46fa85baa0afc04cae486b82fe
Rémi Vezy <VEZY@users.noreply.github.com>
2023-09-04 15:56:21 +0200

    Make website

---
+++ Julia_course/content/_includes/welcome.md
@@ -0,0 +1,12 @@
+---
+layout: "md.jlmd"
+---
+
+# Julia course: from total beginner to power user
+
+This repository is a collection of resources to teach everything about the Julia programming language.
+
+These resources are used in the [Julia: from total beginner to power user](https://www.youtube.com/watch?v=ZZJJgQ2IzQQ&list=PLLiJ249IkzRFxZGALbKy75_ZyHxYCUmuk) Youtube playlist.
+
+> **Acknowledgement** \\
+> The design of this website is based on _**Computational Thinking**, a live online Julia/Pluto textbook._ [(computationalthinking.mit.edu)](https://computationalthinking.mit.edu), using [this template](https://github.com/greimel/pluto-course-template).
---
+++ Julia_course/content/sidebar data.jl
@@ -0,0 +1,21 @@
+Dict(
+    :main => [
+        "welcome" => collections["welcome"].pages,
+        "Course" => collections["course"].pages,
+        # "Module 2: Social Science & Data Science" => collections["module2"].pages,
+        #"Module 3: Climate Science" => collections["module3"].pages,
+
+    ],
+    :about => Dict(
+        :authors => [
+            (name = "Rémi Vezy", url = "https://www.remivezy.com"),
+        ],
+        :title => "Julia course",
+        :subtitle => "From Total Beginner To Power User",
+        :term => "",
+        :institution => "",
+        :institution_url => "",
+        :institution_logo => "julia-logo-color.svg",
+        :institution_logo_darkmode => "julia-logo-dark.svg"
+    )
+)
